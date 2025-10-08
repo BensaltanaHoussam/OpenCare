bc2f37cfe1f1030e16be5f0fe95742fe984c3a7f
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-08 16:20:45 +0100

    Add Patient entity and project configuration files

---
+++ OpenCare/src/main/java/com/opencare/entities/Patient.java
@@ -0,0 +1,103 @@
+package com.opencare.entities;
+
+public class Patient {
+    private int id ;
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String numeroSecuriteSociale;
+    private String telephone;
+    private String adresse;
+    private String mutuelle;
+    private String antecedents;
+    private String allergies;
+    private String traitementsEnCours;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getNumeroSecuriteSociale() {
+        return numeroSecuriteSociale;
+    }
+
+    public void setNumeroSecuriteSociale(String numeroSecuriteSociale) {
+        this.numeroSecuriteSociale = numeroSecuriteSociale;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getMutuelle() {
+        return mutuelle;
+    }
+
+    public void setMutuelle(String mutuelle) {
+        this.mutuelle = mutuelle;
+    }
+
+    public String getAntecedents() {
+        return antecedents;
+    }
+
+    public void setAntecedents(String antecedents) {
+        this.antecedents = antecedents;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getTraitementsEnCours() {
+        return traitementsEnCours;
+    }
+
+    public void setTraitementsEnCours(String traitementsEnCours) {
+        this.traitementsEnCours = traitementsEnCours;
+    }
+}
