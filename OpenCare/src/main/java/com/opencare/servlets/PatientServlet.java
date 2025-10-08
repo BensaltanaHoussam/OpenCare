a4e7de068be5d17297dce1eff56cc51d7902b17d
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-08 15:22:52 +0100

    Enhance patient registration form and backend processing

--- OpenCare/src/main/java/com/opencare/servlets/PatientServlet.java
+++ OpenCare/src/main/java/com/opencare/servlets/PatientServlet.java
@@ -2,6 +2,11 @@ package com.opencare.servlets;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import com.opencare.utils.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import java.io.IOException;
 
@@ -11,18 +16,17 @@ public class PatientServlet extends HttpServlet {
         String nom = request.getParameter("nom");
         String prenom = request.getParameter("prenom");
         String dateNaissance = request.getParameter("date_naissance");
-        String nss = request.getParameter("nss");
+        String nss = request.getParameter("numero_securite_sociale");
         String telephone = request.getParameter("telephone");
         String adresse = request.getParameter("adresse");
         String mutuelle = request.getParameter("mutuelle");
         String antecedents = request.getParameter("antecedents");
         String allergies = request.getParameter("allergies");
-        String traitements = request.getParameter("traitements");
-
+        String traitements = request.getParameter("traitements_en_cours");
         response.setContentType("text/html;charset=UTF-8");
 
         try (Connection conn = DBUtil.getConnection()) {
-            String sql = "INSERT INTO patients(nom, prenom, date_naissance, nss, telephone, adresse, mutuelle, antecedents, allergies, traitements) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO patients(nom, prenom, date_naissance, numero_securite_sociale, telephone, adresse, mutuelle, antecedents, allergies, traitements_en_cours) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, nom);
             stmt.setString(2, prenom);
